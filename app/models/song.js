ad09c8ae2d22974330de1a78c11a89187b021595
Zachary Liu <liu.zachary@gmail.com>
2014-02-22 12:14:21 -0500

    Add list of recordings to SongSchema

--- app/models/song.js
+++ app/models/song.js
@@ -12,6 +12,10 @@ var Schema = mongoose.Schema;
 var SongSchema = new Schema({
     title: String,
     artist: String,
+    recordings: [{
+        user: ObjectId,
+        path: String
+    }],
     dogeAddress: String,
     dogeBalance: Number
 });
