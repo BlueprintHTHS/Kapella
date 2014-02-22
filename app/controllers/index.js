49c908c144009fb0ffb352ec1ce3082ab4e4c86d
Matthew Hsu <gears88@gmail.com>
2014-02-22 12:44:49 -0500

    [fix] removed extra code in index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,9 +5,3 @@ exports.render = function(req, res) {
         user: req.user ? JSON.stringify(req.user) : 'null'
     });
 };
-
-exports.render = function(req, res) {
-    res.render('index/dogetest', {
-        user: req.user ? JSON.stringify(req.user) : 'null'
-    });
-};
